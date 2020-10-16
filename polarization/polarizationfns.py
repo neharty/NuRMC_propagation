334c40ebecea1c5e5d86129db49bd9e7062d82b5
nick <neharty@udel.edu>
2020-10-16 00:57:14 -0400

    working on rotation tests

--- polarization/polarizationfns.py
+++ polarization/polarizationfns.py
@@ -2,12 +2,32 @@ import numpy as np
 import scipy.linalg as spl
 
 def get_n(s,n):
+    '''
+        computes eigenspeeds of birefringent 
+        material as roots of a polynomial, from 
+        Fresnel's equation
+
+        s: direction (unit vector)
+        n: array of speeds, n_i = sqrt(eps_i)
+
+        NOTE: equation is only valid for diagonalized epsilon matrix
+    '''
+
     sx, sy, sz = s[0], s[1], s[2]
     vx, vy, vz = 1/n[0], 1/n[1], 1/n[2]
     v1, v2 = np.sqrt(np.roots([sx**2+sy**2+sz**2, -sx**2*(vy**2+vz**2)-sy**2*(vx**2+vz**2)-sz**2*(vx**2+vy**2), (sx*vy*vz)**2+(sy*vx*vz)**2+(sz*vx*vy)**2]))
     return 1/v1, 1/v2
 
 def get_null(s, eps, rcond=1e-8):
+    '''
+        computes null space of L(n,s) 
+        matrix, which gives E polarizations
+        
+        s: direction (unit vector)
+        eps: permittivity matrix
+        rcond: tolerance for null space solver (see numpy doc)
+    '''
+    
     n1, n2 = get_n(s,np.sqrt(np.diag(eps)))
     s1, s2, s3 = s
 
@@ -18,4 +38,37 @@ def get_null(s, eps, rcond=1e-8):
         [eps[1,0] + n2**2*s2*s1, eps[1,1] - n2**2*(s1**2+s3**2), eps[1,2] + n2**2*s2*s3],
         [eps[2,0] + n2**2*s3*s1, eps[2,1] + n2**2*s3*s2, eps[2,2] - n2**2*(s1**2+s2**2)]])
     return spl.null_space(L1, rcond=rcond), spl.null_space(L2, rcond=rcond)
+
+def get_basis(eps):
+    '''
+        computes rotation matrix as 
+        eigenvectors of polarization matrix
+
+        eps: permittivity matrix
+    '''
+
+    ofd = eps[1,0]
+
+    v1 = np.array([2*ofd, eps[1,1] - eps[0,0] - np.sqrt((eps[0,0]-eps[1,1])**2 + 4*ofd**2), 0])
+    v1 = v1/spl.norm(v1)
+
+    v2 = np.array([2*ofd, eps[1,1] - eps[0,0] + np.sqrt((eps[0,0]-eps[1,1])**2 + 4*ofd**2), 0])
+    v2 = v2/spl.norm(v2)
+
+    v3 = np.array([0.,0.,1.])
+    
+    R = np.zeros((3,3))
+    R[:,0] = v1[:]
+    R[:,1] = v2[:]
+    R[:,2] = v3[:]
+
+    #vals, vects = spl.eig(eps)
+    #print(vects)
+
+    return R
+
+def rotate_basis(v, eps):
+    '''
+        
+    '''
 
--- polarization/rotationtest.py
+++ polarization/rotationtest.py
@@ -1,14 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import polarizationfns as pf
 
-eps = np.diag([3.1, 3.5, 3.8])
-ofd = 0.5
+eps = np.diag([3.1, 3.3, 3.5])
+ofd = 1e-2
 eps[0, 1] = ofd
 eps[1, 0] = ofd
 
-vals, vects = np.linalg.eig(eps)
-print(vals)
-print((eps[0,0] +  eps[1,1] + np.sqrt((eps[0,0]-eps[1,1])**2 + 4*ofd**2))/2)
-print((eps[0,0] +  eps[1,1] - np.sqrt((eps[0,0]-eps[1,1])**2 + 4*ofd**2))/2)
-print(vects)
-print(vects.T@eps@vects)
+R = pf.get_basis(eps)
+print(R)
+print(R.T@eps@R)
+
+#initial direction
+kinit = np.array([np.sin(np.pi/4), 0, np.cos(np.pi/4)])
+
+#intial eps array (already diagonal)
+epsinit = eps = np.diag([3.12, 3.3, 3.5])
+
+#eigenspeeds (for s, p respectively)
+n_s, n_p = pf.get_n(kinit, np.sqrt(np.diag(epsinit)))
+
+#polarization vectors
+p, s = pf.get_null(kinit, epsinit)
+
+#p in plane, s out of plane
+p = p.T[0]
+s = s.T[0]
+print(p)
+print(s)
+
+# plane of incidence is the x-z plane, plane normal is
+pn = np.array([0,1,0])
+
+#rotated k
+kir = R@kinit
+
+#rotated plane 
+pnr = R@pn
+
+#rotated s, p vectors
+sr = R@s
+pr = R@p
+
+print(pr)
+print(sr)
