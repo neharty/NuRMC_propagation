2b01d18ec05ee1f1093f52495aaf262a7b80b90f
nick <neharty@udel.edu>
2020-11-02 15:00:27 -0500

    full sim

--- polarization/snell_uniaxial_full.py
+++ polarization/snell_uniaxial_full.py
@@ -23,57 +23,82 @@ parser.add_argument('z0', type=float,
 #theta0 = args.theta0
 #z0 = args.z0
 '''
-# fit data for permittivity tensor
-epsdata = pd.read_csv('./epsdata/evals_vs_depth.csv')
-print(epsdata)
-zdepth = -np.array(epsdata['Nominal Depth'])
-e1 = 3.157 + 0.034*np.array(epsdata['E1'])
-e2 = 3.157 + 0.034*np.array(epsdata['E2'])
-e3 = 3.157 + 0.034*np.array(epsdata['E3'])
 
-e12 = (e1+e2)/2
-
-nxy = np.sqrt(e12)
-nz = np.sqrt(e3)
-
-del e1, e2, e3, e12, epsdata
-
-def nfit(z, a, b, c):
-    z0 = z[0]
-    return a + b*np.exp((z-z0)/c)
-
-nxyparams, junk = curve_fit(nfit, zdepth, nxy)
-nzparams, junk = curve_fit(nfit, zdepth, nz)
 '''
-plt.plot(zdepth, nxy, '.', label=r'$n_{o}$')
-plt.plot(zdepth, nz, '.', label=r'$n_{e}$')
-plt.plot(zdepth, nfit(zdepth, *nxyparams), label = r'$n_{o}$ fit')
-plt.plot(zdepth, nfit(zdepth, *nzparams), label = r'$n_{e}$ fit')
-plt.xlim([zdepth[0], zdepth[-1]])
-plt.legend()
-plt.xlabel('nominal depth [m]')
-plt.ylabel('index of refraction')
-plt.savefig('icemodel.png')
-plt.clf()
-'''
+    # fit data for permittivity tensor
+    epsdata = pd.read_csv('./epsdata/evals_vs_depth.csv')
+    print(epsdata)
+    zdepth = -np.array(epsdata['Nominal Depth'])
+    e1 = 3.157 + 0.034*np.array(epsdata['E1'])
+    e2 = 3.157 + 0.034*np.array(epsdata['E2'])
+    e3 = 3.157 + 0.034*np.array(epsdata['E3'])
 
-zs = np.linspace(0, -2700, num=2701)
-plt.plot(zs, nfit(zs, *nxyparams), label='n_o')
-plt.plot(zs, nfit(zs, *nzparams), label='n_e')
-plt.show()
-plt.clf()
+    e12 = (e1+e2)/2
+
+    nxy = np.sqrt(e12)
+    nz = np.sqrt(e3)
+
+    del e1, e2, e3, e12, epsdata
+
+    def nfit(z, a, b, c):
+        z0 = z[0]
+        return a + b*np.exp((z-z0)/c)
+
+    nxyparams, junk = curve_fit(nfit, zdepth, nxy)
+    nzparams, junk = curve_fit(nfit, zdepth, nz)
+
+    plt.plot(zdepth, nxy, '.', label=r'$n_{o}$')
+    plt.plot(zdepth, nz, '.', label=r'$n_{e}$')
+    plt.plot(zdepth, nfit(zdepth, *nxyparams), label = r'$n_{o}$ fit')
+    plt.plot(zdepth, nfit(zdepth, *nzparams), label = r'$n_{e}$ fit')
+    plt.xlim([zdepth[0], zdepth[-1]])
+    plt.legend()
+    plt.xlabel('nominal depth [m]')
+    plt.ylabel('index of refraction')
+    plt.savefig('icemodel.png')
+    plt.clf()
+
+    zs = np.linspace(0, -2700, num=2701)
+    plt.plot(zs, nfit(zs, *nxyparams), label='n_o')
+    plt.plot(zs, nfit(zs, *nzparams), label='n_e')
+    plt.show()
+    plt.clf()
+    '''
+# ice parameters
+nss = 1.35
+nd = 1.78
+c = 0.0132
+
+d = 0.1
 
 def nz(z):
     # z index of refraction function
-    z0 = zdepth[0]
-    a, b, c = nzparams
-    return a + b*np.exp((z-z0)/c) 
+    # from nice8 ARA model
+    a = nd        
+    b = nss - nd
+    n1 = a + b*np.exp(-d*c)
+    n2 = 1
+
+    if  z > d:
+        return 1
+    if np.abs(z) <= d:
+        return (n2-n1)*z/(2*d) +(n2+n1)/2
+    if z < -d:
+        return a + b*np.exp(z*c) 
 
 def no(z):
     # x-y plane index of refraction function
-    z0 = zdepth[0]
-    a, b, c = nxyparams
-    return a + b*np.exp((z-z0)/c)
+    a = nd
+    b = nss - nd
+    n1 = a + b*np.exp(-d*c)
+    n2 = 1
+
+    if  z > d:
+        return 1
+    if np.abs(z) <= d:
+        return (n2-n1)*z/(2*d) + (n2+n1)/2
+    if z < -d:
+        return 0.99*nz(z)
 
 def eps(z):
     # epsilon is diagonal
@@ -81,15 +106,22 @@ def eps(z):
 
 def dnodz(z):
     #derivative of x-y index of refraction
-    z0 = zdepth[0]
-    a, b, c = nxyparams
-    return b*np.exp((z-z0)/c)/c
+    a = nd
+    b = nss - nd
+    n1 = a + b*np.exp(-d*c)
+    n2 = 1
+
+    if z > d:
+        return 0
+    if np.abs(z) <= d:
+        return (n2-n1)/(2*d)
+    if z < -d:
+        return 0.99*dnzdz(z)
 
 def dnzdz(z):
     #derivative of z index of refraction
-    z0 = zdepth[0]
-    a, b, c = nzparams
-    return (b/c)*np.exp((z-z0)/c)
+    b = nss - nd
+    return b*c*np.exp(z*c)
 
 def ns(z):
     #s-polarization index of refraction
@@ -105,11 +137,24 @@ def npp(theta, z):
 
 def dnpdtheta(theta, z):
     #partial of np w.r.t. theta
-    return no(z)*nz(z)*np.sin(theta)*np.cos(theta)*(nz(z)**2-no(z)**2)/(nz(z)**2*np.cos(theta)**2+no(z)**2*np.sin(theta)**2)**1.5
+    if z >= -d:
+        return 0
+    if z < -d:
+        return no(z)*nz(z)*np.sin(theta)*np.cos(theta)*(nz(z)**2-no(z)**2)/(nz(z)**2*np.cos(theta)**2+no(z)**2*np.sin(theta)**2)**1.5
 
 def dnpdz(theta, z):
     #partial of np w.r.t. z
-    return (no(z)**3*dnzdz(z)*np.sin(theta)**2+dnodz(z)*nz(z)**3*np.cos(theta)**2)/(nz(z)**2*np.cos(theta)**2+no(z)**2*np.sin(theta)**2)**1.5
+    a = nd
+    b = nss - nd
+    n1 = a + b*np.exp(-d*c)
+    n2 = 1
+
+    if z > d:
+        return 0
+    if np.abs(z) <= d:
+        return (n2-n1)/(2*d)
+    if z < -d:
+        return (no(z)**3*dnzdz(z)*np.sin(theta)**2+dnodz(z)*nz(z)**3*np.cos(theta)**2)/(nz(z)**2*np.cos(theta)**2+no(z)**2*np.sin(theta)**2)**1.5
 
 def podes(t, y):
     # odes for p-polarization
@@ -121,33 +166,47 @@ def sodes(t,y):
     # form is [d(theta)/dr, dzdr]
     return [-dnsdz(y[1])/(ns(y[1])), 1/np.tan(y[0])]
 
-def pfn(theta, rmax, z0, zm, dl):
-    sol = solve_ivp(podes, [0, rmax], [theta, z0], method='RK45', max_step = dl)
+def pfn(theta, rmax, z0, zm, dr):
+    sol = solve_ivp(podes, [0, rmax], [theta, z0], method='DOP853', max_step = dr)
     zsol = sol.y[1,-1]
     return np.abs(zsol - zm)
 
-def sfn(theta, rmax, z0, zm, dl):
-    sol = solve_ivp(sodes, [0, rmax], [theta, z0], method='RK45', max_step = dl)
+def sfn(theta, rmax, z0, zm, dr):
+    sol = solve_ivp(sodes, [0, rmax], [theta, z0], method='DOP853', max_step = dr)
     zsol = sol.y[1,-1]
     return np.abs(zsol - zm)
 
 rmax = 1000
-z0 = -25
-zm = -20
-dl = 10
+#z0 = -200
+zm = -200
+dr = 10
+dz = 10
 
-pminsol = minimize(pfn, (np.pi - np.arctan((zm-z0)/rmax)), args=(rmax, z0, zm, dl))
-podesol = solve_ivp(podes, [0, rmax], [pminsol.x[0], z0], method='RK45', max_step = dl)
+def initialangle(zd, z0):
+    if zd-z0 < 0:
+        return np.pi/4
+    if zd-z0 >= 0:
+        return np.pi/2 - np.arctan((zm-z0)/rmax)
 
-sminsol = minimize(sfn, (np.pi - np.arctan((zm-z0)/rmax)), args=(rmax, z0, zm, dl))
-sodesol = solve_ivp(sodes, [0, rmax], [sminsol.x[0], z0], method='RK45', max_step = dl)
+zarr = np.linspace(-1, -2000, num=20)
+for j in range(len(zarr)):
+    z0 = zarr[j]
+    pminsol = minimize(pfn, (initialangle(zm, z0)), args=(rmax, z0, zm, dr), tol = 1e-5)
+    print('pminsol', pminsol.success, pminsol.message)
+    podesol = solve_ivp(podes, [0, rmax], [pminsol.x[0], z0], method='DOP853', max_step = dr)
+    tp = (10/3)*np.trapz([npp(podesol.y[0,i], podesol.y[1,i])*np.abs(1/np.sin(podesol.y[0,i])) for i in range(len(podesol.t))],  podesol.t)
+    
+    sminsol = minimize(sfn, (initialangle(zm,z0)), args=(rmax, z0, zm, dr), tol=1e-5)
+    print('sminsol', sminsol.success, pminsol.message)
+    sodesol = solve_ivp(sodes, [0, rmax], [sminsol.x[0], z0], method='DOP853', max_step = dr)
+    ts = (10/3)*np.trapz([npp(sodesol.y[0,i], sodesol.y[1,i])*np.abs(1/np.sin(sodesol.y[0,i])) for i in range(len(sodesol.t))],  sodesol.t)
 
-#tab = pd.DataFrame(np.array([pminsol.x[0], sminsol.x[0]]), index_col=['p-pol', 's-pol'])
-print('launch angles: ',pminsol.x[0], sminsol.x[0])
-print('final depth: ', podesol.y[1,-1], sodesol.y[1,-1])
-print(no(-2700))
-plt.plot(podesol.t, podesol.y[1], label='p-pol')
-plt.plot(sodesol.t, sodesol.y[1], label='s-pol')
+    print('p-s dt [ns]:', tp-ts)
+    print('launch angles: ',pminsol.x[0], sminsol.x[0])
+    print('final depth: ', podesol.y[1,-1], sodesol.y[1,-1])
+    plt.plot(podesol.t, podesol.y[1], label='p-pol', color='blue')
+    plt.plot(sodesol.t, sodesol.y[1], label='s-pol', color='orange')
+
 plt.plot(0, z0, '*', label = 'source')
 plt.plot(rmax, zm, '^', label = 'antenna')
 plt.xlabel('r')
