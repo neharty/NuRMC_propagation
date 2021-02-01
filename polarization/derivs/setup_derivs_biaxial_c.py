e08ab188aeb38d54e9f4917306f608a79c0c1866
nick <neharty@udel.edu>
2021-01-31 21:41:39 -0500

    restarting directory

--- polarization/derivs/setup_derivs_biaxial_c.py
+++
@@ -1,4 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(ext_modules=cythonize('derivs_biaxial_c.pyx'))
--- polarization/vector class/vector_class_test.py
+++
@@ -1,6 +0,0 @@
-import vector
-
-a = vector.vector([1,1,1])
-print(a.getVect())
-print(a.getDir())
-print(a.getMag())
